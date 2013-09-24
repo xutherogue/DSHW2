c75953f9b8f341463a2527091eb93a470302d188
parth patel <parth022@cpp.cs.rutgers.edu>
2013-09-24 14:11:11 -0400

    filename

--- server.c
+++ server.c
@@ -1,6 +1,43 @@
-nclude <sys/socket.h>
+#include <sys/socket.h>:wq
 #include <netinet/in.h>
 #include <stdio.h>
+#include <errno.h>
+
+
+#include<sys/types.h>
+#include<sys/socket.h>
+#include <stdio.h>
+#include<unistd.h>
+#include<strings.h>
+#include<stdlib.h>
+#include<netinet/in.h>
+#include<sys/time.h>
+#include<sys/types.h>
+#include<sys/select.h>
+#include<string.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <time.h>
+
+
+void err_exit()//displays error 
+{
+	perror("server");
+	exit(1);
+}
+
+struct ack
+{
+	uint16_t opcode;
+	uint16_t blocknum;
+
+}mypacket;
+
+
 
 int main(int argc, char**argv)
 {
@@ -8,33 +45,82 @@ int main(int argc, char**argv)
    struct sockaddr_in servaddr,cliaddr;
    socklen_t len;
    char mesg[1000];
-	int *reva;
-	reva=(int *) malloc(size*sizeof(int));
-   sockfd=socket(AF_INET,SOCK_DGRAM,0);
-
-   bzero(&servaddr,sizeof(servaddr));
-   servaddr.sin_family = AF_INET;
-   servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
-   servaddr.sin_port=htons(32000);
-   bind(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
-
-   for (;;)
-   {
-      len = sizeof(cliaddr);
-      n = recvfrom(sockfd,reva,1000,0,(struct sockaddr *)&cliaddr,&len);
-      sendto(sockfd,mesg,n,0,(struct sockaddr *)&cliaddr,sizeof(cliaddr));
-      printf("-------------------------------------------------------\n");
-      mesg[n] = 0;
-      printf("Received the following:\n");
-      printf("%s",mesg);
-      printf("-------------------------------------------------------\n");
-   }
-
-
-
-
-
-
-
-
-}
+	char reva[3000];// dynamic array
+	//reva=(int *) malloc(size*sizeof(int));
+	//  	 sockfd=socket(AF_INET,SOCK_DGRAM,0);
+	//
+	//
+	//  	 if (argc != 2) {
+	//  	 	fprintf(stderr, "error insert port number\n", argv[0]);
+	//  	 		exit(0);
+	//  	 		    }
+	//
+	//
+	//
+	//
+	//  	 		       bzero(&servaddr,sizeof(servaddr));
+	//  	 		          servaddr.sin_family = AF_INET;
+	//  	 		             servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+	//  	 		                servaddr.sin_port=htons(4567);
+	//  	 		                   
+	//  	 		                   	if(bind(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr))<0){
+	//  	 		                   		err_exit();}
+	//
+	//
+	//  	 		                   			
+	//
+	//
+	//
+	//
+	//  	 		                   			   for (;;)
+	//  	 		                   			      {
+	//  	 		                   			            len = sizeof(cliaddr);
+	//  	 		                   			                  n = recvfrom(sockfd,reva,sizeof(*reva),0,(struct sockaddr *)&cliaddr,&len);
+	//  	 		                   			                        
+	//  	 		                   			                              sendto(sockfd,mesg,n,0,(struct sockaddr *)&cliaddr,sizeof(cliaddr));
+	//  	 		                   			                                    printf("-------------------------------------------------------\n");
+	//  	 		                   			                                          mesg[n] = 0;
+	//  	 		                   			                                                printf("Received the following:\n");
+	//  	 		                   			                                                      printf("%s",mesg);
+	//  	 		                   			                                                            printf("-------------------------------------------------------\n");
+	//  	 		                   			                                                              
+	//
+	//  	 		                   			                                                                  
+	//  	 		                   			                                                                  	char *p;
+	//  	 		                   			                                                                  	    p = strtok(reva+4, " ");
+	//  	 		                   			                                                                  	     char str[128];
+	//  	 		                   			                                                                  	         if(p)
+	//  	 		                   			                                                                  	            {
+	//  	 		                   			                                                                  	                sprintf(str,"%s\n", p);//saves the file name
+	//  	 		                   			                                                                  	                  }
+	//  	 		                   			                                                                  	                      printf("%s", str);
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//  	 		                   			                                                                  	                            else
+	//  	 		                   			                                                                  	                                  {err_exit();}
+	//  	 		                   			                                                                  	                                    
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//  	 		                   			                                                                  	                                     }
+	//
+	//  	 		                   			                                                                  	                                     	
+	//
+	//
+	//
+	//
+	//
+	//
+	//  	 		                   			                                                                  	                                     	}
